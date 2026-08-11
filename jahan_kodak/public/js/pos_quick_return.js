785361c8b5270a17a99405bf6ffae06c05b33c6b
Anonymous <mmsafi5207@gmail.com>
2026-08-11 23:09:27 +0430

    added the red, green and black color to the pos

--- jahan_kodak/public/js/pos_quick_return.js
+++ jahan_kodak/public/js/pos_quick_return.js
@@ -138,9 +138,118 @@ frappe.provide("jahan_kodak.pos");
 		});
 	}
 
+	function overridePosPaymentTotals() {
+		if (!window.erpnext || !erpnext.PointOfSale || !erpnext.PointOfSale.Payment) {
+			return;
+		}
+
+		if (erpnext.PointOfSale.Payment.prototype._totals_color_overridden) {
+			return;
+		}
+
+		erpnext.PointOfSale.Payment.prototype._totals_color_overridden = true;
+
+		erpnext.PointOfSale.Payment.prototype.update_totals_section = function (doc) {
+			if (!doc) doc = this.events.get_frm().doc;
+			const paid_amount = doc.paid_amount || 0;
+			const grand_total = cint(frappe.sys_defaults.disable_rounded_total)
+				? doc.grand_total
+				: doc.rounded_total;
+			const remaining = grand_total - paid_amount;
+			const change = doc.change_amount || (remaining <= 0 ? -1 * remaining : undefined);
+			const currency = doc.currency;
+			const label = __("Change Amount");
+
+			if (!this.$totals || !this.$totals.is(":visible")) {
+				return;
+			}
+
+			const precision = frappe.defaults.get_default("currency_precision") || 2;
+			const rem_flt = flt(remaining, precision);
+
+			let value_style = "font-weight: 600;";
+			if (rem_flt === 0) {
+				value_style += " color: inherit;";
+			} else if (rem_flt < 0) {
+				value_style += " color: #28a745;";
+			} else {
+				value_style += " color: #dc3545;";
+			}
+
+			this.$totals.html(
+				`<div class="col">
+					<div class="total-label">${__("Grand Total")}</div>
+					<div class="value">${format_currency(grand_total, currency)}</div>
+				</div>
+				<div class="seperator-y"></div>
+				<div class="col">
+					<div class="total-label">${__("Paid Amount")}</div>
+					<div class="value">${format_currency(paid_amount, currency)}</div>
+				</div>
+				<div class="seperator-y"></div>
+				<div class="col">
+					<div class="total-label">${label}</div>
+					<div class="value" style="${value_style}">${format_currency(change || remaining, currency)}</div>
+				</div>`
+			);
+		};
+
+		if (window.cur_pos && window.cur_pos.payment) {
+			window.cur_pos.payment.update_totals_section();
+		}
+	}
+
+	function applyItemCardVisibilityToggle() {
+		const route = frappe.get_route_str();
+		if (route !== "point-of-sale" && !window.location.hash.includes("point-of-sale")) {
+			return;
+		}
+
+		if (!window.cur_pos) {
+			return;
+		}
+
+		const $itemsSelector = $(".items-selector");
+		if (!$itemsSelector.length) {
+			return;
+		}
+
+		if ($("#pos-hide-item-cards-style").length === 0) {
+			$("head").append(`
+				<style id="pos-hide-item-cards-style">
+					.items-selector.hide-item-cards-view .items-container,
+					.items-selector.hide-item-cards-view .filter-section .label,
+					.items-selector.hide-item-cards-view .filter-section .item-group-field {
+						display: none !important;
+					}
+					.items-selector.hide-item-cards-view .filter-section .search-field {
+						width: 100% !important;
+						flex: 1 1 100% !important;
+					}
+					.items-selector.hide-item-cards-view .filter-section {
+						margin-bottom: 0 !important;
+					}
+				</style>
+			`);
+		}
+
+		const settings = window.cur_pos.settings || {};
+		const hideCards = cint(settings.hide_item_cards) === 1;
+
+		if (hideCards) {
+			$itemsSelector.addClass("hide-item-cards-view");
+		} else {
+			$itemsSelector.removeClass("hide-item-cards-view");
+		}
+	}
+
 	setInterval(renderQuickReturn, 1000);
+	setInterval(overridePosPaymentTotals, 1000);
+	setInterval(applyItemCardVisibilityToggle, 1000);
 
 	$(document).on("page-change route", function () {
 		setTimeout(renderQuickReturn, 500);
+		setTimeout(overridePosPaymentTotals, 500);
+		setTimeout(applyItemCardVisibilityToggle, 500);
 	});
 })();
